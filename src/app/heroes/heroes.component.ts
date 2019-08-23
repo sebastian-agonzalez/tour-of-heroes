ace0e2e6c1371f669b62e063a696691f1af810af
Sebastian Gonzalez <onemogen8@gmail.com>
2019-08-23 00:14:33 -0300

    23

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,7 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
   
-  hero: Hero = { id: 1, name: 'Windstorm'}; 
+  // hero: Hero = { id: 1, name: 'Windstorm'}; 
 
   selectedHero: Hero;
   
