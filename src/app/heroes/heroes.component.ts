7b78e851cd5b9da6b79e31350617e23e8395f81a
Sebastian Gonzalez <onemogen8@gmail.com>
2019-08-23 00:11:52 -0300

    23/8

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,6 +11,12 @@ export class HeroesComponent implements OnInit {
   
   hero: Hero = { id: 1, name: 'Windstorm'}; 
 
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   heroes = HEROES;
 
   constructor() { }
